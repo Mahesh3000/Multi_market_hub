7f46e4f441d72a214d7e44ddaed653e16154763c
maneesh-settipeta <maneeshsettipeta@gmail.com>
2025-03-12 15:47:02 -0500

    udpating signin

--- User-Service/src/UserSchema.ts
+++ User-Service/src/UserSchema.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 const userSchema = z.object({
-    id: z.string(),
     firstname: z.string().min(2, "Name must be 2 characters long"),
     lastname: z.string(),
     email: z.string().email("Invalid Email Format"),
