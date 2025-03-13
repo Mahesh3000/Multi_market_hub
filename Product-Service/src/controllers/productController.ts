a6942dd1de6dcb8ef039d2fb387f0459792f43e1
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-03-13 10:37:02 -0500

    added User schma

--- Product-Service/src/controllers/productController.ts
+++ Product-Service/src/controllers/productController.ts
@@ -4,10 +4,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching users" });
+  }
+};
+
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    // const users = await prisma.user.findMany();
+    // res.json(users);
   } catch (error) {
     res.status(500).json({ error: "Error fetching users" });
   }
--- Product-Service/src/routes/productRoutes.ts
+++ Product-Service/src/routes/productRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllUsers } from "../controllers/productController";
+import { getAllUsers, getProducts } from "../controllers/productController";
 
 const router = express.Router();
 
 // Get all users
 router.get("/users", getAllUsers);
+
+router.get("/products", getProducts);
 
 export default router;
